f6ea3491145ad8523c03a4f368e52ad44bb5cd48
jhalfpennycodes <jhalfpennycodes@gmail.com>
2025-07-23 16:53:07 +0100

    Add path to database again

--- backend/flask-server/config.py
+++ backend/flask-server/config.py
@@ -1,13 +1,10 @@
 import os
 import secrets
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
 class Config:
-    # Use DATABASE_URL env var if set (e.g. for Postgres), else fallback to /tmp/db.sqlite3 on Render
     SQLALCHEMY_DATABASE_URI = os.getenv(
         'DATABASE_URL',
-        'sqlite:////tmp/db.sqlite3'  # note 4 slashes for absolute path
+        'sqlite:////tmp/db.sqlite3'  # fallback to tmp folder
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', secrets.token_hex(16))
