e765e122a72ce576f992b517192727307838c021
jhalfpennycodes <jhalfpennycodes@gmail.com>
2025-09-02 16:12:49 +0100

    Image name change

--- frontend/src/common/Home.jsx
+++ frontend/src/common/Home.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import HomeCard from "./Card";
-import profileTree from "../Assets/FT_View_Tree copy.png";
+import profileTree from "../Assets/FT_View_Tree.png";
 import sideControls from "../Assets/FT_Side_Controls.png";
 import profileOpen from "../Assets/FT_Profile_Open.png";
 import { Container, Row, Col } from "react-bootstrap";
