55e3c223052828b4557444a462362965ed1903d1
jhalfpennycodes <jhalfpennycodes@gmail.com>
2025-10-20 16:31:00 +0100

    Remove pointless node modules

--- backend/flask-server/config.py
+++ backend/flask-server/config.py
@@ -1,6 +1,7 @@
 import os
 import secrets
 
+
 # Get the absolute path to the backend/flask-server directory
 basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '.'))
 
@@ -10,5 +11,4 @@ class Config:
         'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', secrets.token_hex(16))
-    CORS_ORIGINS = os.getenv('CORS_ORIGINS', '').split(',')
+    SECRET_KEY = os.getenv('SECRET_KEY')
